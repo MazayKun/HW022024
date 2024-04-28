addb2947ba6a48c327ba58b6e3166f17ea90ead4
MazayKun <kir99916@gmail.com>
2024-04-28 13:08:30 +0300

    Init commit

---
+++ echo/src/test/java/ru/mikheev/kirill/otus/echo/EchoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikheev.kirill.otus.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
